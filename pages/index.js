a2034c25d69cdf57b52505dd61e756c16ec08e31
Tudor Alexandru <tudorale2005@gmail.com>
2022-01-05 19:45:02 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,128 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Nft from "../components/Nft";
 
 export default function Home() {
+
+  const nfts = [
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+    {name: "Name", src: "/images/placeholder.jpg"},
+  ]
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Hide and Seek</title>
+        <meta name="description" content="Hide and Seek" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className={styles.heading}>hide and seek</h1>
+      <img className={styles.nft1} src="/images/nft1.png" alt="NFT" />
+      <img className={styles.nft2} src="/images/nft2.png" alt="NFT" />
+      <img className={styles.nft3} src="/images/nft3.png" alt="NFT" />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <img className={styles.nft4} src="/images/nft4.png" alt="NFT" />
+      <img className={styles.nft5} src="/images/nft1.png" alt="NFT" />
+      <img className={styles.nft6} src="/images/nft2.png" alt="NFT" />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <div className={styles.nftContainer}>
+        {
+          nfts.map((data) => {
+            return (
+              <Nft name={`${data.name}`} key={6 * Math.random(99999) + Math.random(99999)} src={`${data.src}`} />
+            )
+          })
+        }
+      </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <img className={styles.all} src="/images/all.png" alt="All NFTs" />
     </div>
   )
 }
